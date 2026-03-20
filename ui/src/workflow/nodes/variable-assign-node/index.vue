5cb29c958e4f748ad0ddd27423aecf32ade80a65
王良 <841369634@qq.com>
2026-03-20 22:29:15 +0800

    样式调整

--- ui/src/workflow/nodes/variable-assign-node/index.vue
+++ ui/src/workflow/nodes/variable-assign-node/index.vue
@@ -123,10 +123,8 @@
               <CodemirrorEditor
                 title="JSON"
                 v-model="item.value"
-                :style="{
-                  height: '100px',
-                  width: '155px',
-                }"
+                class="w-full"
+                style="height: 100px"
                 @submitDialog="(val: string) => (form_data.variable_list[index].value = val)"
               />
             </el-form-item>
@@ -139,7 +137,7 @@
                 required: true,
               }"
             >
-              <el-select v-model="item.value" style="width: 155px" :teleported="false">
+              <el-select v-model="item.value" :teleported="false">
                 <el-option label="true" :value="true" />
                 <el-option label="false" :value="false" />
               </el-select>
@@ -191,7 +189,7 @@ const props = defineProps<{ nodeModel: any }>()
 
 const typeOptions = ['string', 'num', 'json', 'bool']
 const targetTypeOptions = [
-  { label: t('workflow.nodes.variableAssignNode.convertType'), key: '' },
+  { label: t('workflow.nodes.variableAssignNode.doNotConvert'), key: '' },
   { label: 'string', key: 'string' },
   { label: 'int', key: 'int' },
   { label: 'float', key: 'float' },
