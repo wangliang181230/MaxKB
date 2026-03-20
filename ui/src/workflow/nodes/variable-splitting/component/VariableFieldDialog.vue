949fc007871c6eedc97e4f1028a639a3336eefda
王良 <841369634@qq.com>
2026-03-20 10:19:42 +0800

    测试

--- ui/src/workflow/nodes/variable-splitting/component/VariableFieldDialog.vue
+++ ui/src/workflow/nodes/variable-splitting/component/VariableFieldDialog.vue
@@ -57,11 +57,11 @@
             >
             <el-tooltip
               effect="dark"
-              :content="
-                $t('workflow.nodes.variableSplittingNode.expression.tooltip')
-              "
               placement="right"
             >
+              <template #content>
+                <span v-html="$t('workflow.nodes.variableSplittingNode.expression.tooltip')"></span>
+              </template>
               <AppIcon iconName="app-warning" class="app-warning-icon"></AppIcon>
             </el-tooltip>
           </div>
