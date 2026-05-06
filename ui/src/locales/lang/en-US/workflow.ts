79598a8fc7beaa043fb823afff50c94b860e5434
王良 <841369634@qq.com>
2026-05-06 15:41:29 +0800

    02-feat: Configurable output of `reason` for `Intent recognition`

--- ui/src/locales/lang/en-US/workflow.ts
+++ ui/src/locales/lang/en-US/workflow.ts
@@ -241,9 +241,9 @@ You are a master of problem optimization, adept at accurately inferring user int
 
 ##Skills
 ###Skill 1: Optimizing Problems
-2. Receive user input questions.
-3. Carefully analyze the meaning of the problem based on the context.
-4. Output optimized problems.
+1. Receive user input questions.
+2. Carefully analyze the meaning of the problem based on the context.
+3. Output optimized problems.
 
 ##Limitations:
 -Only return the optimized problem without any additional explanation or clarification.
@@ -482,6 +482,7 @@ You are a master of problem optimization, adept at accurately inferring user int
       classify: {
         label: 'Intent classify',
       },
+      output_reason: 'Output Reason',
       input: {
         label: 'Input',
       },
--- ui/src/workflow/nodes/intent-classify-node/index.vue
+++ ui/src/workflow/nodes/intent-classify-node/index.vue
@@ -169,6 +169,19 @@
             </div>
           </div>
         </el-form-item>
+        <el-form-item>
+          <div class="flex-between w-full">
+            <div>
+              <span>{{ $t('workflow.nodes.intentNode.output_reason') }}</span>
+            </div>
+            <div>
+              <el-switch
+                size="small"
+                v-model="form_data.output_reason"
+              />
+            </div>
+          </div>
+        </el-form-item>
       </el-form>
     </el-card>
 
@@ -329,6 +342,7 @@ const form = {
   ],
   dialogue_number: 1,
   content_list: [],
+  output_reason: true,
 }
 
 function refreshParam(data: any) {
@@ -348,6 +362,9 @@ const form_data = computed({
       }
       if (!props.nodeModel.properties.node_data.model_id_reference) {
         set(props.nodeModel.properties.node_data, 'model_id_reference', [])
+      }
+      if (props.nodeModel.properties.node_data.output_reason == null) {
+        set(props.nodeModel.properties.node_data, 'output_reason', true)
       }
       return props.nodeModel.properties.node_data
     } else {
