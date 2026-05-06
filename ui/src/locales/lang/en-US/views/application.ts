9fbac71da54f1fe8c48ecad3610114dce2e84945
王良 <841369634@qq.com>
2026-05-06 15:41:29 +0800

    03-feat: Custom prompt for `Intent recognition`

--- ui/src/locales/lang/en-US/views/application.ts
+++ ui/src/locales/lang/en-US/views/application.ts
@@ -106,6 +106,11 @@ After disabling, the long-term memory of the conversation users will be cleared,
          - Please use concise and professional language to answer the user's question.
          `,
     },
+    prompt_template: {
+      label: 'Prompt Template',
+      requiredMessage: 'Please enter Prompt template',
+      tooltip: 'Please pay attention to the placeholders in the template: {classification_list}、{user_input}、{output_json}',
+    },
     historyRecord: {
       label: 'Chat History',
     },
