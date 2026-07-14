b379925b6ff9c1a42acb15f5b4a8c8b8ca6433a1
王良 <841369634@qq.com>
2026-07-14 17:21:46 +0800

    My 04 - feat: 新增 “读取长期记忆” 的节点

---
+++ apps/application/flow/step_node/read_long_term_memory_node/__init__.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+"""
+    @project: MaxKB
+    @Author：AI Assistant
+    @file： __init__.py
+    @date：2026/07/14
+    @desc: 读取长期记忆节点模块
+"""
+from .impl import *
---
+++ ui/src/workflow/icons/read-long-term-memory-node-icon.vue
@@ -0,0 +1,6 @@
+<template>
+  <el-avatar shape="square" style="background: #8B5CF6">
+    <img src="@/assets/workflow/icon_memory.svg" style="width: 75%" alt="" />
+  </el-avatar>
+</template>
+<script setup lang="ts"></script>
