896e257ba12e820720db3317d0c8f2ad4bcfdc13
王良 <841369634@qq.com>
2026-07-14 17:21:46 +0800

    My 04 - feat: 新增 “读取长期记忆” 的节点

---
+++ apps/application/flow/step_node/read_long_term_memory_node/impl/__init__.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+"""
+    @project: maxkb
+    @Author：虎
+    @file： __init__.py
+    @date：2024/6/11 15:35
+    @desc:
+"""
+from .base_read_long_term_memory_node import BaseReadLongTermMemoryNode
