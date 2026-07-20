44a7656bc82b2658b98a993d9df555865b290cf1
王良 <841369634@qq.com>
2026-07-20 17:06:40 +0800

    01-fix: Fix NPE

--- apps/application/flow/step_node/search_knowledge_node/impl/base_search_knowledge_node.py
+++ apps/application/flow/step_node/search_knowledge_node/impl/base_search_knowledge_node.py
@@ -121,6 +121,7 @@ class BaseSearchKnowledgeNode(ISearchKnowledgeStepNode):
             return get_none_result(question)
         paragraph_list = self.list_paragraph(embedding_list, vector)
         result = [self.reset_paragraph(paragraph, embedding_list) for paragraph in paragraph_list]
+        result = [r for r in result if r is not None]  # 去除None
         result = sorted(result, key=lambda p: p.get('similarity'), reverse=True)
         return NodeResult({'paragraph_list': result,
                            'is_hit_handling_method_list': [row for row in result if row.get('is_hit_handling_method')],
