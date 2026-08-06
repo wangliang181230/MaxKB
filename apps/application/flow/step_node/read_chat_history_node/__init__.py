419e851b852f90d7d954044eea7f9c1624197da3
王良 <841369634@qq.com>
2026-08-06 09:42:10 +0800

    My 05 - feat: 新增 “读取历史对话” 的节点

---
+++ apps/application/flow/step_node/read_chat_history_node/__init__.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+"""
+    @project: MaxKB
+    @Author：AI Assistant
+    @file： __init__.py
+    @date：2026/08/06
+    @desc:
+"""
+from .impl.base_read_chat_history_node import BaseReadChatHistoryNode
